d0e1e2ddb76abd4023e6296d647555b61c601d0d
Silas <krausesilas@gmail.com>
2020-09-30 17:24:13 +0200

    Add README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+# Blogpost2Audio
+
+## 📦 Initial Setup
+1. Copy .env file (`cp .env.example .env`) and add the necessary info
+2. Run `npm install`
+3. Build the frontent (`cd frontend && npm i && npm run build`)
+
+
+## 🚀 Start App
+`npm run start`
+
+
+## Deployment
+This App can be deployed to IBM Cloud using cloud foundry.
--- frontend/src/aws.js
+++ frontend/src/aws.js
@@ -3,7 +3,7 @@ import Polly from "aws-sdk/clients/polly"
 
 AWS.config.region = 'eu-central-1';
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: 'eu-central-1:df97c608-06bc-49f5-abe5-c9639bc52387',
+    IdentityPoolId: process.env.AWS_IDENTITY_POOL_ID,
 });
 
 export const PollyClient = new Polly({
